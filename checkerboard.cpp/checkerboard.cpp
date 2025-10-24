f0ff20d88e1a6a5953790c5f3001788caeeb493b
f2025-0961 <f2025-0961@bnu.edu.pk>
2025-10-23 22:28:29 -0700

    Lab5

---
+++ checkerboard.cpp/checkerboard.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row, columns;
+    cout << "Enter number of rows:" << endl;
+    cin >> row;
+    cout << "Enter number of colums:" << endl;
+    cin >> columns;
+    return 0;
+}
---
+++ primeNumbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int startingNum, endNum;
+    cout << "Enter starting point of range and ending point:" << endl;
+    cin >> startingNum >> endNum;
+    int odd= 1;
+    if (odd %2!=0 )
+    {
+     
+    }
+    for (int i = startingNum; i <= endNum; i++)  
+    {
+        
+    }
+
+    return 0;
+}
