499910191bf3dd2f82be25423aedee83a63e1ff9
f2025-0961 <f2025-0961@bnu.edu.pk>
2025-10-24 09:43:14 -0700

    checkerboard

--- checkerboard.cpp/checkerboard.cpp
+++ checkerboard.cpp/checkerboard.cpp
@@ -8,5 +8,19 @@ int main()
     cin >> row;
     cout << "Enter number of colums:" << endl;
     cin >> columns;
+    for (int i = 1; i <= row; i++)
+    {
+        if (i%2==0)
+        {
+           cout << " ";
+        }
+        
+        for (int j = 1; j <= columns; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
